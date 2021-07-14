7401bc42682a6259370e5bf9a8d0493a9ea9a972
Armando <borelliarmando12@gmail.com>
2021-07-14 11:54:46 -0500

    Added readme and eliminated react front end

--- job_board_app/urls.py
+++ job_board_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('user/indeed-auth', views.indeed_auth)
+    path('', views.index)
 ]
--- job_board_app/views.py
+++ job_board_app/views.py
@@ -1,18 +1,4 @@
 from django.shortcuts import render, HttpResponse, redirect
-import requests
-import json
 
 def index(request):
-    return redirect('http://localhost:3000/')
-
-def indeed_auth(request):
-    print("indeed_auth request:")
-
-    if request.method == 'GET':
-        response = requests.get("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam/json/named.sport_hitting_tm.bam?league_list_id='mlb'&game_type='R'&season='2021'&player_id='545361'")
-        print(response)
-        return HttpResponse(response)
-    else:
-        print("request method is not GET")
-
-    # return redirect('http://localhost:3000/')
+    return HttpResponse("hello");

12887aa1dea52e04921ab25062fbcabbdc9fc860
Armando <borelliarmando12@gmail.com>
2021-07-15 00:51:40 -0500

    Added models

--- job_board_app/models.py
+++ job_board_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import date, datetime
 
 # Create your models here.
+
+class User(models.Model):
+
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Jobs(models.Model):
+
+    status = models.CharField(max_length=20)
+    title = models.CharField(max_length=50)
+    company = models.CharField(max_length=50)
+    url = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    date_submitted = models.DateField(default=date.today)
+    user_jobs = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

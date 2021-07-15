e99c89e429831cbba71721ae394eae67c449ffe1
Armando <borelliarmando12@gmail.com>
2021-07-14 19:43:41 -0500

    Added working job search API and display job results in page

--- job_board_app/urls.py
+++ job_board_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('jobs', views.jobs),
+    path('search_job', views.search_job)
 ]
--- job_board_app/views.py
+++ job_board_app/views.py
@@ -1,4 +1,50 @@
 from django.shortcuts import render, HttpResponse, redirect
+import requests
+import sys
+import dotenv
+import os
+
 
 def index(request):
-    return HttpResponse("hello");
+    print("env variables:")
+    dotenv.read_dotenv()
+    var = os.environ.get("api_app_id")
+    print(var)
+    return HttpResponse("hello")
+
+# render jobs page
+def jobs(request):
+    return render(request, 'jobs.html')
+
+# makes API call to indeed.com for jobs and then re render the page with the response from the API
+def search_job(request):
+    print("inside search_job")
+
+    dotenv.read_dotenv()
+    the_id = os.environ.get('api_app_id')
+    the_key = os.environ.get('api_key')
+
+    # response = requests.get("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam/json/named.sport_hitting_tm.bam?league_list_id='mlb'&game_type='R'&season='2021'&player_id='545361'")
+    # response = requests.get(f"http://api.adzuna.com/v1/api/jobs/us/search/1?app_id={the_id}&app_key={the_key}&results_per_page=1&what=javascript%20developer&content-type=application/json")
+    response = requests.get(f"http://api.adzuna.com:80/v1/api/jobs/us/search/1?app_id={the_id}&app_key={the_key}&results_per_page=5&what=javascript%20developer&what_exclude=java&where=nc&content-type=application/json")
+    print("response:")
+    print(response.text)
+    print("data:")
+    data = response.json()
+    print(data)
+    results = data["results"]
+
+    # get each job data
+    jobs = []
+
+    for job in results:
+        temp_obj = {}
+        temp_obj["title"] = job["title"]
+        temp_obj["company"] = job["company"]["display_name"]
+        temp_obj["location"] = job["location"]["display_name"]
+        temp_obj["url"] = job["redirect_url"]
+        temp_obj["description"] = job["description"]
+        jobs.append(temp_obj)
+
+
+    return render(request, 'jobs.html', { "jobs": jobs })

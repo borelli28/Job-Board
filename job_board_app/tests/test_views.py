280925a79073690c265f7579a09488ac83bc213f
Armando <borelliarmando12@gmail.com>
2021-07-16 18:48:56 -0500

    Created job instance for views.py test cases

--- job_board_app/tests/test_views.py
+++ job_board_app/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from job_board_app.models import *
+from job_board_app.models import User, Jobs
 import json
 
 class TestViews(TestCase):
@@ -8,6 +8,10 @@ class TestViews(TestCase):
     def setUp(self):
         # create user instance to test views from
         User.objects.create(email="test@test", password="123")
+        user = User.objects.get(id=1)
+
+        # create a job instance for testing purposes
+        job = Jobs.objects.create(status="Applied", title="Software Dev", company="Cheap Labor Inc.", url="https://borelliarmando.com/", location="Austin, TX", user_jobs=user)
 
         self.client = Client()
 
@@ -17,7 +21,6 @@ class TestViews(TestCase):
     def test_index_view_get(self):
         response = self.client.get(self.index_url)
         self.assertEquals(response.status_code, 200)
-        # self.assertTemplateUsed(response, 'job_board_app/')
 
     def test_jobs_view_get(self):
         response = self.client.get(self.jobs_url)

7af52c85f52a20a8ba7adeb8fa6d9e1024f5c184
Armando <borelliarmando12@gmail.com>
2021-07-16 18:34:44 -0500

    Added test case for views.py that check for server response and template rendering. Also added comments to explain what and how we are testing for

--- job_board_app/tests/test_views.py
+++ job_board_app/tests/test_views.py
@@ -10,9 +10,16 @@ class TestViews(TestCase):
         User.objects.create(email="test@test", password="123")
 
         self.client = Client()
+
         self.index_url = reverse("index")
+        self.jobs_url = reverse("render_jobs")
 
     def test_index_view_get(self):
         response = self.client.get(self.index_url)
         self.assertEquals(response.status_code, 200)
         # self.assertTemplateUsed(response, 'job_board_app/')
+
+    def test_jobs_view_get(self):
+        response = self.client.get(self.jobs_url)
+        self.assertEquals(response.status_code, 200) # test that method returns an OK server response
+        self.assertTemplateUsed(response, 'jobs.html')  # test that method renders the right template

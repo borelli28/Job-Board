d9bff793934d83f0bfcd335ab82d27e862f3f4a3
Armando <borelliarmando12@gmail.com>
2021-07-16 16:17:57 -0500

    second url test

--- job_board_app/tests/test_urls.py
+++ job_board_app/tests/test_urls.py
@@ -2,9 +2,15 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from job_board_app.views import *
 
+#   ./manage.py test job_board_app                  *activate virtualenv to run tests*
+
 class TestUrls(SimpleTestCase):
 
+    # test url by: checking the url name resolves to the views method
     def test_index_url(self):
         url = reverse("index")
-        print(resolve(url))
         self.assertEquals(resolve(url).func, index)
+
+    def test_render_jobs_url(self):
+        url = reverse("render_jobs")
+        self.assertEquals(resolve(url).func, jobs)

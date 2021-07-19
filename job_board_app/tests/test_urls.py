e1a63a5ba2b2cc1296da38a2b5fa75c44ca3c96e
Armando <borelliarmando12@gmail.com>
2021-07-18 19:24:50 -0500

    Added error messages to all URL's test cases

--- job_board_app/tests/test_urls.py
+++ job_board_app/tests/test_urls.py
@@ -9,51 +9,51 @@ class TestUrls(SimpleTestCase):
     # test url by: checking the url name resolves to the views method
     def test_index_url(self):
         url = reverse("index")
-        self.assertEquals(resolve(url).func, index)
+        self.assertEquals(resolve(url).func, index, "index url does not link to the method [index] in views")
 
     def test_render_jobs_url(self):
         url = reverse("render_jobs")
-        self.assertEquals(resolve(url).func, jobs)
+        self.assertEquals(resolve(url).func, jobs, "render_jobs url does not link to the method [jobs] in views")
 
     def test_job_search_logic_url(self):
         url = reverse("job_search_logic")
-        self.assertEquals(resolve(url).func, search_job)
+        self.assertEquals(resolve(url).func, search_job, "job_search_logic url does not link to the method [search_job] in views")
 
     def test_render_tracker_app_url(self):
         url = reverse("render_tracker_app")
-        self.assertEquals(resolve(url).func, tracker_app)
+        self.assertEquals(resolve(url).func, tracker_app, "render_tracker_app url does not link to the method [tracker_app] in views")
 
     def test_save_job_info_url(self):
         url = reverse("save_job_info")
-        self.assertEquals(resolve(url).func, set_job)
+        self.assertEquals(resolve(url).func, set_job, "save_job_info url does not link to the method [set_job] in views")
 
     def test_go_to_job_url(self):
         url = reverse("go_to_job")
-        self.assertEquals(resolve(url).func, go_to_job)
+        self.assertEquals(resolve(url).func, go_to_job, "go_to_job url does not link to the method [go_to_job] in views")
 
     def test_viewed_job_handler_url(self):
         url = reverse("viewed_job_handler", args=["1"])
-        self.assertEquals(resolve(url).func, viewed_jobs_handler)
+        self.assertEquals(resolve(url).func, viewed_jobs_handler, "viewed_job_handler url does not link to the method [viewed_job_handler] in views")
 
     def test_edit_job_form_url(self):
         url = reverse("edit_job_form", args=["1"])
-        self.assertEquals(resolve(url).func, edit_job)
+        self.assertEquals(resolve(url).func, edit_job, "edit_job_form url does not link to the method [edit_job] in views")
 
     def test_update_job_logic_url(self):
         url = reverse("update_job_logic", args=["1"])
-        self.assertEquals(resolve(url).func, update_job)
+        self.assertEquals(resolve(url).func, update_job, "update_job_logic url does not link to the method [update_job] in views")
 
     def test_delete_job_url(self):
         url = reverse("delete_job", args=["1"])
-        self.assertEquals(resolve(url).func, delete_job)
+        self.assertEquals(resolve(url).func, delete_job, "delete_job url does not link to the method [delete_job] in views")
 
     def test_render_job_note_url(self):
         url = reverse("render_job_note", args=["1"])
-        self.assertEquals(resolve(url).func, job_note)
+        self.assertEquals(resolve(url).func, job_note, "render_job_note url does not link to the method [job_note] in views")
 
     def test_update_job_note_url(self):
         url = reverse("update_job_note", args=["1"])
-        self.assertEquals(resolve(url).func, update_note)
+        self.assertEquals(resolve(url).func, update_note, "update_job_note url does not link to the method [update_note] in views")
 
     def test_new_job_url(self):
         url = reverse("new_job_form")
